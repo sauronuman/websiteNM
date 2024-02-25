e6f35b70ae4e1de286ab4e201c93b14affdf141e
D Gartland <dgartland021@c2ken.net>
2024-02-25 10:04:56 +0000

    Initial commit of website files

---
+++ _vti_cnf/textsizer.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|19 Oct 2023 10:49:09 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|C2KEN\\nmichlewicz182
+vti_modifiedby:SR|C2KEN\\nmichlewicz182
+vti_timecreated:TR|19 Oct 2023 10:49:09 -0000
+vti_cacheddtm:TX|19 Oct 2023 10:49:09 -0000
+vti_filesize:IR|1270
+vti_backlinkinfo:VX|HTML\\ Hot\\ Beans\\ Web/Web\\ Development\\ Courses.html HTML\\ Hot\\ Beans\\ Web/Existing\\ Trainees.html HTML\\ Hot\\ Beans\\ Web/On-Line\\ application\\ Form.html Template\\ new\\ updated.dwt HTML\\ Hot\\ Beans\\ Web/Job\\ Specifications\\ &\\ Entry\\ requirements.html Index.html
